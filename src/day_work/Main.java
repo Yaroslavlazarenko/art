0e361d7e7471676e568f822f6a3c8a1889dc1748
Azerfael <yaroslav.lazarenko.odessa@gmail.com>
2022-03-15 17:27:51 +0200

    yin-yung

---
+++ src/day_work/Main.java
@@ -0,0 +1,41 @@
+package day_work;
+
+public class Main {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+
+    public static void main(String[] args) {
+        System.out.println(ANSI_CYAN + "██" + ANSI_YELLOW + "██████████████████████████████" + ANSI_CYAN + "████████████████████████████████");
+        System.out.println("████" + ANSI_YELLOW + "████████████████████████████" + ANSI_CYAN + "██████████████████████████████" + ANSI_YELLOW + "██");
+        System.out.println(ANSI_CYAN + "██████" + ANSI_YELLOW + "████████████████" + ANSI_BLACK + "████████████████████" + ANSI_CYAN + "██████████████████" + ANSI_YELLOW + "████");
+        System.out.println(ANSI_CYAN + "████████" + ANSI_YELLOW + "██████████" + ANSI_BLACK + "████" + ANSI_RESET + "██████████████████" + ANSI_BLACK + "██████" + ANSI_CYAN + "████████████" + ANSI_YELLOW + "██████");
+        System.out.println(ANSI_CYAN + "██████████" + ANSI_YELLOW + "████" + ANSI_BLACK + "████" + ANSI_RESET + "████████████████████████" + ANSI_BLACK + "████████" + ANSI_CYAN + "██████" + ANSI_YELLOW + "████████");
+        System.out.println(ANSI_CYAN + "████████████" + ANSI_BLACK + "██" + ANSI_RESET + "██████████████████████████████" + ANSI_BLACK + "████████" + ANSI_CYAN + "██" + ANSI_YELLOW + "██████████");
+        System.out.println(ANSI_CYAN + "██████████" + ANSI_BLACK + "██" + ANSI_RESET + "████████████████████████████████" + ANSI_BLACK + "██████████" + ANSI_YELLOW + "██████████");
+        System.out.println(ANSI_CYAN + "████████" + ANSI_BLACK + "██" + ANSI_RESET + "████████████████████" + ANSI_BLACK + "████" + ANSI_RESET + "████████████" + ANSI_BLACK + "██████████" + ANSI_YELLOW + "████████");
+        System.out.println(ANSI_CYAN + "████████" + ANSI_BLACK + "██" + ANSI_RESET + "██████████████████" + ANSI_BLACK + "████████" + ANSI_RESET + "██████████" + ANSI_BLACK + "██████████" + ANSI_YELLOW + "████████");
+        System.out.println(ANSI_CYAN + "██████" + ANSI_BLACK + "██" + ANSI_RESET + "████████████████████" + ANSI_BLACK + "████████" + ANSI_RESET + "██████████" + ANSI_BLACK + "████████████" + ANSI_YELLOW + "██████");
+        System.out.println(ANSI_CYAN + "██████" + ANSI_BLACK + "██" + ANSI_RESET + "██████████████████████" + ANSI_BLACK + "████" + ANSI_RESET + "████████████" + ANSI_BLACK + "████████████" + ANSI_YELLOW + "██████");
+        System.out.println(ANSI_CYAN + "████" + ANSI_BLACK + "██" + ANSI_RESET + "██████████████████████████████████████" + ANSI_BLACK + "████████████████" + ANSI_YELLOW + "████");
+        System.out.println(ANSI_CYAN + "████" + ANSI_BLACK + "██" + ANSI_RESET + "██████████████████████████████████████" + ANSI_BLACK + "████████████████" + ANSI_YELLOW + "████");
+        System.out.println(ANSI_CYAN + "████" + ANSI_BLACK + "██" + ANSI_RESET + "████████████████████████████████████" + ANSI_BLACK + "██████████████████" + ANSI_YELLOW + "████");
+        System.out.println(ANSI_CYAN + "████" + ANSI_BLACK + "██" + ANSI_RESET + "████████████████████████████████" + ANSI_BLACK + "██████████████████████" + ANSI_YELLOW + "████");
+        System.out.println(ANSI_YELLOW + "████" + ANSI_BLACK + "██" + ANSI_RESET + "████████████████████" + ANSI_BLACK + "██████████████████████████████████" + ANSI_CYAN + "████");
+        System.out.println(ANSI_YELLOW + "████" + ANSI_BLACK + "██" + ANSI_RESET + "████████████████" + ANSI_BLACK + "██████████████████████████████████████" + ANSI_CYAN + "████");
+        System.out.println(ANSI_YELLOW + "████" + ANSI_BLACK + "██" + ANSI_RESET + "██████████████" + ANSI_BLACK + "████████████████████████████████████████" + ANSI_CYAN + "████");
+        System.out.println(ANSI_YELLOW + "████" + ANSI_BLACK + "██" + ANSI_RESET + "██████████████" + ANSI_BLACK + "████████████████████████████████████████" + ANSI_CYAN + "████");
+        System.out.println(ANSI_YELLOW + "██████" + ANSI_BLACK + "██" + ANSI_RESET + "██████████" + ANSI_BLACK + "████████████" + ANSI_RESET + "████" + ANSI_BLACK + "████████████████████████" + ANSI_CYAN + "██████");
+        System.out.println(ANSI_YELLOW + "██████" + ANSI_BLACK + "██" + ANSI_RESET + "██████████" + ANSI_BLACK + "██████████" + ANSI_RESET + "████████" + ANSI_BLACK + "██████████████████████" + ANSI_CYAN + "██████");
+        System.out.println(ANSI_YELLOW + "████████" + ANSI_BLACK + "██" + ANSI_RESET + "████████" + ANSI_BLACK + "██████████" + ANSI_RESET + "████████" + ANSI_BLACK + "████████████████████" + ANSI_CYAN + "████████");
+        System.out.println(ANSI_YELLOW + "████████" + ANSI_BLACK + "██" + ANSI_RESET + "████████" + ANSI_BLACK + "████████████" + ANSI_RESET + "████" + ANSI_BLACK + "██████████████████████" + ANSI_CYAN + "████████");
+        System.out.println(ANSI_YELLOW + "██████████" + ANSI_BLACK + "██" + ANSI_RESET + "████████" + ANSI_BLACK + "██████████████████████████████████" + ANSI_CYAN + "██████████");
+        System.out.println(ANSI_YELLOW + "██████████" + ANSI_CYAN + "██" + ANSI_BLACK + "██" + ANSI_RESET + "██████" + ANSI_BLACK + "████████████████████████████████"  + ANSI_CYAN + "████████████");
+        System.out.println(ANSI_YELLOW + "████████" + ANSI_CYAN + "██████" + ANSI_BLACK + "████" + ANSI_RESET + "████" + ANSI_BLACK + "████████████████████████████" + ANSI_YELLOW + "████" + ANSI_CYAN + "██████████");
+        System.out.println(ANSI_YELLOW + "██████" + ANSI_CYAN + "████████████" + ANSI_BLACK + "████" + ANSI_RESET + "██" + ANSI_BLACK + "██████████████████████" + ANSI_YELLOW + "██████████" + ANSI_CYAN + "████████");
+        System.out.println(ANSI_YELLOW + "████" + ANSI_CYAN + "██████████████████" + ANSI_BLACK + "████████████████████" + ANSI_YELLOW + "████████████████" + ANSI_CYAN + "██████");
+
+    }
+}
